54c3f550422e766f6eda90d0e19273a44d6dbba3
Nandhini <nandhini.sridhar02@gmail.com>
2021-07-27 11:12:14 -0700

    started api call

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//fot new york times thing
+const baseUrl = 'https://api.nytimes.com/svc/topstories/v2/home.json?';//api-key=yourkey'
+const key = '1ImkjMiVW3f6WMAIzdTlOXD7B1fND6CH';
+let url = baseUrl + 'api-key=' + key;
+
+fetch(url).then(function(result){
+    return result.json();
+}).then(function(json){
+    displayResults(json)
+})
+
+function displayResults(){
+    const articles = json.response.docs;
+      
+}
+
 function displayMessage(){
     //alert("hello!");
     console.log("entered function");
@@ -10,3 +26,4 @@ function displayMessage(){
     //alert(listItemText);
     //listItem.innerHTML
 }
+
